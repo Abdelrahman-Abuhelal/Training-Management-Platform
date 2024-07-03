de2aab6a4f382b6eeb58d324f57994e33989e596
Abdelrahman Abu hilal <Abdelrahman.Hilal@exalt.ps>
2024-07-03 11:02:41 +0300

    User Management Update

--- frontend/src/pages/adminPortal/UserManagement.jsx
+++ frontend/src/pages/adminPortal/UserManagement.jsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import {
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow,
-  Paper, Button, Dialog, DialogActions, DialogContent, DialogTitle,
-  TextField, Checkbox, FormControlLabel
+  Paper, Button, Dialog, DialogActions, DialogContent, DialogTitle, Box, IconButton,
+  TextField, Checkbox, TableSortLabel, Toolbar, Typography, MenuItem, FormControl, Select
 } from '@mui/material';
 import axios from 'axios';
-
+import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import SearchComponent from '../../components/Search';
 const UserManagement = () => {
+  const baseUrl = import.meta.env.VITE_PORT_URL;
   const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [order, setOrder] = useState('asc');
+  const [orderBy, setOrderBy] = useState('userEmail');
   const [open, setOpen] = useState(false);
   const [currentUser, setCurrentUser] = useState(null);
-  const baseUrl = import.meta.env.VITE_PORT_URL;
+  const [newUser, setNewUser] = useState({
+    userEmail: '',
+    userFirstName: '',
+    userLastName: '',
+    userUsername: '',
+    userRole: ''
+  });
 
   useEffect(() => {
-    // Fetch users from API
-    axios.get(`${baseUrl}/api/v1/admin/users`)  // Replace with your API endpoint
-      .then(response => setUsers(response.data))
+    axios.get(`${baseUrl}/api/v1/admin/users`)
+      .then(response => {
+        setUsers(response.data);
+        setFilteredUsers(response.data);
+      })
       .catch(error => console.error(error));
   }, []);
 
@@ -24,126 +40,243 @@ const UserManagement = () => {
     setOpen(true);
   };
 
+  const handleDelete = (userId) => {
+    axios.delete(`/api/users/${userId}`)
+      .then(() => {
+        setUsers(users.filter(user => user.userId !== userId));
+        setFilteredUsers(filteredUsers.filter(user => user.userId !== userId));
+      })
+      .catch(error => console.error(error));
+  };
+
   const handleClose = () => {
     setOpen(false);
     setCurrentUser(null);
   };
 
   const handleSave = () => {
-    // Save user details
-    axios.put(`/api/users/${currentUser.userId}`, currentUser)  // Replace with your API endpoint
-      .then(response => {
-        setUsers(users.map(user => (user.userId === currentUser.userId ? currentUser : user)));
-        handleClose();
-      })
-      .catch(error => console.error(error));
+    if (currentUser) {
+      axios.put(`/api/users/${currentUser.userId}`, currentUser)
+        .then(response => {
+          setUsers(users.map(user => (user.userId === currentUser.userId ? currentUser : user)));
+          setFilteredUsers(filteredUsers.map(user => (user.userId === currentUser.userId ? currentUser : user)));
+          handleClose();
+        })
+        .catch(error => console.error(error));
+    } else {
+      axios.post(`/api/users`, newUser)
+        .then(response => {
+          setUsers([...users, response.data]);
+          setFilteredUsers([...filteredUsers, response.data]);
+          handleClose();
+        })
+        .catch(error => console.error(error));
+    }
   };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCurrentUser({ ...currentUser, [name]: value });
+    if (currentUser) {
+      setCurrentUser({ ...currentUser, [name]: value });
+    } else {
+      setNewUser({ ...newUser, [name]: value });
+    }
   };
 
-  const handleCheckboxChange = (e) => {
-    const { name, checked } = e.target;
-    setCurrentUser({ ...currentUser, [name]: checked });
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+    const term = e.target.value.toLowerCase();
+    setFilteredUsers(users.filter(user =>
+      user.userEmail.toLowerCase().includes(term) ||
+      user.userFirstName.toLowerCase().includes(term) ||
+      user.userLastName.toLowerCase().includes(term) ||
+      user.userUsername.toLowerCase().includes(term) ||
+      String(user.userEnabled).toLowerCase().includes(term)
+    ));
+  };
+
+  const handleSort = (property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+    const sortedUsers = [...filteredUsers].sort((a, b) => {
+      if (a[property] < b[property]) return order === 'asc' ? -1 : 1;
+      if (a[property] > b[property]) return order === 'asc' ? 1 : -1;
+      return 0;
+    });
+    setFilteredUsers(sortedUsers);
   };
 
   return (
-    <div>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Email</TableCell>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Username</TableCell>
-              <TableCell>Role</TableCell>
-              <TableCell>Enabled</TableCell>
-              <TableCell>Action</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {users.map(user => (
-              <TableRow key={user.userId}>
-                <TableCell>{user.userEmail}</TableCell>
-                <TableCell>{user.userFirstName}</TableCell>
-                <TableCell>{user.userLastName}</TableCell>
-                <TableCell>{user.userUsername}</TableCell>
-                <TableCell>{user.userRole}</TableCell>
-                <TableCell>
-                  <Checkbox checked={user.userEnabled} disabled />
-                </TableCell>
-                <TableCell>
-                  <Button onClick={() => handleEdit(user)}>Edit</Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Paper elevation={3} sx={{ p: 2, m: 3, width: "95%", maxWidth: 1800 }}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flex: '1 2 100%' }}>
+            User Management
+          </Typography>
+          <SearchComponent sx={{ maxWidth: '350px', height: '36px' }}
+            searchTerm={searchTerm} onSearchChange={handleSearch} />
+          <Box style={{ marginLeft: '10px' }}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setOpen(true)}
+              startIcon={<AddIcon />}
+              sx={{ minWidth: '150px', height: '36px' }}
+            >
+              New User
+            </Button>
+          </Box>
+        </Toolbar>
+        <Paper sx={{ border: "1px solid #ccc", p: "1rem" }}>
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userEmail'}
+                      direction={orderBy === 'userEmail' ? order : 'asc'}
+                      onClick={() => handleSort('userEmail')}
+                    >
+                      Email
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userFirstName'}
+                      direction={orderBy === 'userFirstName' ? order : 'asc'}
+                      onClick={() => handleSort('userFirstName')}
+                    >
+                      First Name
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userLastName'}
+                      direction={orderBy === 'userLastName' ? order : 'asc'}
+                      onClick={() => handleSort('userLastName')}
+                    >
+                      Last Name
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userUsername'}
+                      direction={orderBy === 'userUsername' ? order : 'asc'}
+                      onClick={() => handleSort('userUsername')}
+                    >
+                      Username
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userRole'}
+                      direction={orderBy === 'userRole' ? order : 'asc'}
+                      onClick={() => handleSort('userRole')}
+                    >
+                      Role
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>
+                    <TableSortLabel
+                      active={orderBy === 'userEnabled'}
+                      direction={orderBy === 'userEnabled' ? order : 'asc'}
+                      onClick={() => handleSort('userEnabled')}
+                    >
+                      Enabled
+                    </TableSortLabel>
+                  </TableCell>
+                  <TableCell>Actions</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {filteredUsers.map(user => (
+                  <TableRow key={user.userId}>
+                    <TableCell>{user.userEmail}</TableCell>
+                    <TableCell>{user.userFirstName}</TableCell>
+                    <TableCell>{user.userLastName}</TableCell>
+                    <TableCell>{user.userUsername}</TableCell>
+                    <TableCell>{user.userRole}</TableCell>
+                    <TableCell>
+                      <Checkbox checked={user.userEnabled} disabled />
+                    </TableCell>
+                    <TableCell>
+                      <IconButton onClick={() => handleEdit(user)} color="primary">
+                        <EditIcon fontSize="medium" />
+                      </IconButton>
+                      <IconButton onClick={() => handleDelete(user.userId)} color="error">
+                        <DeleteIcon fontSize="medium" />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
 
-      <Dialog open={open} onClose={handleClose}>
-        <DialogTitle>Edit User</DialogTitle>
-        <DialogContent>
-          <TextField
-            margin="dense"
-            name="userEmail"
-            label="Email"
-            type="email"
-            fullWidth
-            value={currentUser?.userEmail || ''}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="userFirstName"
-            label="First Name"
-            fullWidth
-            value={currentUser?.userFirstName || ''}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="userLastName"
-            label="Last Name"
-            fullWidth
-            value={currentUser?.userLastName || ''}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="userUsername"
-            label="Username"
-            fullWidth
-            value={currentUser?.userUsername || ''}
-            onChange={handleChange}
-          />
-          <TextField
-            margin="dense"
-            name="userRole"
-            label="Role"
-            fullWidth
-            value={currentUser?.userRole || ''}
-            onChange={handleChange}
-          />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={currentUser?.userEnabled || false}
-                onChange={handleCheckboxChange}
-                name="userEnabled"
-              />
-            }
-            label="Enabled"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleSave}>Save</Button>
-        </DialogActions>
-      </Dialog>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>{currentUser ? "Edit User" : "Add User"}</DialogTitle>
+          <DialogContent>
+            <TextField
+              margin="dense"
+              name="userEmail"
+              label="Email"
+              type="email"
+              fullWidth
+              value={currentUser ? currentUser.userEmail : newUser.userEmail}
+              onChange={handleChange}
+              disabled={!!currentUser}
+            />
+            <TextField
+              margin="dense"
+              name="userFirstName"
+              label="First Name"
+              fullWidth
+              value={currentUser ? currentUser.userFirstName : newUser.userFirstName}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="userLastName"
+              label="Last Name"
+              fullWidth
+              value={currentUser ? currentUser.userLastName : newUser.userLastName}
+              onChange={handleChange}
+            />
+            <TextField
+              margin="dense"
+              name="userUsername"
+              label="Username"
+              fullWidth
+              value={currentUser ? currentUser.userUsername : newUser.userUsername}
+              onChange={handleChange}
+            />
+            <FormControl fullWidth margin="dense">
+              <Select
+                name="userRole"
+                value={currentUser ? currentUser.userRole : newUser.userRole}
+                onChange={handleChange}
+                displayEmpty
+                disabled={!!currentUser}
+              >
+                <MenuItem value="" disabled>
+                  Select Role
+                </MenuItem>
+                <MenuItem value="TRAINEE">Trainee</MenuItem>
+                <MenuItem value="SUPERVISOR">Supervisor</MenuItem>
+                <MenuItem value="SUPER_ADMIN">Admin</MenuItem>
+              </Select>
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleSave}>Save</Button>
+          </DialogActions>
+        </Dialog>
+
+      </Paper>
     </div>
   );
 };
